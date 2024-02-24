50f705890ed8bc79dc2b58ccf0bf8e1d1645deb9
Thom <thom.allan03@googlemail.com>
2024-02-24 22:25:02 +0000

    Change database to sqlite

--- sustainability_website/settings.py
+++ sustainability_website/settings.py
@@ -74,23 +74,10 @@ WSGI_APPLICATION = 'sustainability_website.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'sustainability_website',
-
-        'USER': 'admin',
-
-        'PASSWORD': 'admin',
-
-        'HOST': '127.0.0.1',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
 
 
