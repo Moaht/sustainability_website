032ab1c1711c600a1a11d191474a7f0ad8ab07f3
SMFreeze <117759431+smfreeze@users.noreply.github.com>
2024-02-27 18:44:25 +0000

    Update RegForm.jsx

--- frontend/src/components/RegForm.jsx
+++ frontend/src/components/RegForm.jsx
@@ -20,22 +20,66 @@ class RegForm extends React.Component {
     render() {
       return (
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Enter username</label>
-          <input id="username" name="username" type="text" />
-  
-          <label htmlFor="firstName">Enter your forename</label>
-          <input id="firstName" name="firstName" type="text" />
-
-          <label htmlFor="lastName">Enter your surname</label>
-          <input id="lastName" name="lastName" type="text" />
-
-          <label htmlFor="email">Enter your email</label>
-          <input id="email" name="email" type="email" />
-
-          <label htmlFor="password">Enter your password</label>
-          <input id="password" name="password" type="password" />
-          <button>Send data!</button>
-        </form>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              <b>Username</b>
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="firstName" className="form-label">
+              <b>First Name</b>
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="lastName" className="form-label">
+              <b>Last Name</b>
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              <b>Email</b>
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              <b>Password</b>
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
+          </div>
+        <br></br>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
       );
     }
   }
