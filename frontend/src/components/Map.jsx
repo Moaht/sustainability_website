11cc271aa4eaf89e5635c330f98e92a23e5adcce
oUesio <116362669+oUesio@users.noreply.github.com>
2024-03-13 14:24:42 +0000

    New map page

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./map-style.css";
 import pointerOn from '../images/Pointer_ON.svg'
 import pointerOff from '../images/Pointer_OFF.svg'
 import map from '../images/Map.svg'
-import mapBelow from '../images/MapBelow.svg'
+import tick from '../images/Tick.svg'
+import attach from '../images/Attach.svg'
 
 export default function Map() {
   const [currentBuilding, setCurrentBuilding] = useState('');
@@ -11,6 +12,7 @@ export default function Map() {
   const enterBuilding = (id) => {
     const pointer = document.getElementById(id);
     const current = document.getElementById(currentBuilding);
+    const buildings = {"into-pointer":"INTO", "newman-pointer":"Newman", "streath-pointer":"Streatham Court" , "amory-pointer":"Amory Building", "harrison-pointer":"Harrison Building", "forum-pointer":"Forum", "swiot-pointer":"SWIoT"};
 
     if (currentBuilding !== id) {
       pointer.src = pointerOn;
@@ -19,15 +21,48 @@ export default function Map() {
         current.src = pointerOff;
         current.style.transform = 'scale(1)';
       }
+      document.getElementById("inside-words").innerHTML = "Current Building: "+buildings[id];
+      document.getElementById("all-tasks").innerHTML = `
+        <div class="task" id="task1">
+          <p>Task 1: Read a text and answer a question</p>
+          <button type="button" class="btn btn-primary" id="read">
+            Read
+          </button>
+        </div>
+        <div class="task" id="task2">
+          <p>Task 2: Temp attachment task placeholder</p>
+          <img class="attach" />
+          <img class="tick" />
+        </div>
+        <div class="task" id="task3">
+          <p>Task 3: Temp attachment task placeholder</p>
+          <img class="attach" />
+          <img class="tick" />
+        </div>
+      `;
+      const attachElements = document.getElementsByClassName("attach");
+      const tickElements = document.getElementsByClassName("tick");
+      for (let i = 0; i < attachElements.length; i++) {
+        attachElements[i].src = attach;
+      }
+  
+      for (let i = 0; i < tickElements.length; i++) {
+        tickElements[i].src = tick;
+      }
       setCurrentBuilding(id);
     } else {
       pointer.src = pointerOff;
       pointer.style.transform = 'scale(1)';
+      document.getElementById("inside-words").innerHTML = "Enter a building to view its tasks";
+      document.getElementById("all-tasks").innerHTML = "";
       setCurrentBuilding('');
     }
-
     console.log(currentBuilding);
   };
+
+  useEffect(() => {
+    document.title = 'Map';
+  }, []);
   
   return (
     <div id="main">
@@ -87,8 +122,24 @@ export default function Map() {
           />
           <p>Forum</p>
         </div>
+        <div id="swiot" className="location">
+          <img
+            id="swiot-pointer"
+            className="pointer"
+            src={pointerOff}
+            onClick={() => enterBuilding("swiot-pointer")}
+          />
+          <p>SWIoT</p>
+        </div>
       </div>
-      <img id="below" src={mapBelow} />
+      <div id="task-board">
+          <br></br>
+          <div id="inside">
+            <p id="inside-words">Enter a building to view its tasks</p>
+          </div>
+          <div id="all-tasks">
+          </div>
+      </div>
     </div>
   );
 }
