4252d2c57187461b96fd6671dc0096d9e9b59f58
SMFreeze <117759431+smfreeze@users.noreply.github.com>
2024-03-14 14:42:58 +0000

    Delete components directory

--- components/CSRFToken.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Cookies from 'js-cookie';
-
-const CSRFToken = () => {
-    return (
-        <input id="csrfmiddlewaretoken" name="csrfmiddlewaretoken" type="hidden" value={Cookies.get('csrftoken')} />
-    );
-};
-export default CSRFToken;
