dc7b00676c223ae29ceb4332484545ec4f8a6dff
unknown <rate.bekirov@gmail.com>
2024-03-15 15:38:37 +0000

    add tasks and location to database

--- api/models.py
+++ api/models.py
@@ -12,3 +12,22 @@ class User(AbstractUser):
     email_confirmed = models.BooleanField(default=False)
     def __unicode__(self):
         return self.username
+
+# tasks
+class Task(models.Model):
+    name = models.CharField(max_length=200, null=True) # TODO: maybe change to not null
+    type = models.CharField(max_length=200, null=True) # TODO:maybe change to not null
+    def __str__(self):
+        return self.name
+    
+class Location(models.Model):
+    name=models.CharField(max_length=200, null=True)  # TODO:maybe change to not null
+    def __str__(self):
+        return self.name
+
+
+#locations
+    
+    
+# 
+    
