873e7c4af47f6d34fc736e0f06b9550e4b3fe960
SM Freeze <117759431+smfreeze@users.noreply.github.com>
2024-03-19 15:10:49 +0000

    Fix collection.jsx part 2

--- frontend/src/components/Collection.jsx
+++ frontend/src/components/Collection.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import "./collection-style.css";
-import blob from '../images/Monsters/Blob.svg'
-import sockub from '../images/Monsters/Sockub.svg'
-import chickpick from '../images/Monsters/Chickpick.svg'
-import loom from '../images/Monsters/Loom.svg'
-import gunth from '../images/Monsters/Gunth.svg'
-import vorp from '../images/Monsters/Vorp.svg'
-import bonepos from '../images/Monsters/Bonepos.svg'
-import torrentoise from '../images/Monsters/Torrentoise.svg'
-import veneam from '../images/Monsters/Veneam.svg'
-import sparret from '../images/Monsters/Sparret.svg'
-
+import blob from '../images/Monsters/Blob.svg';
+import sockub from '../images/Monsters/Sockub.svg';
+import chickpick from '../images/Monsters/Chickpick.svg';
+import loom from '../images/Monsters/Loom.svg';
+import gunth from '../images/Monsters/Gunth.svg';
+import vorp from '../images/Monsters/Vorp.svg';
+import bonepos from '../images/Monsters/Bonepos.svg';
+import torrentoise from '../images/Monsters/Torrentoise.svg';
+import veneam from '../images/Monsters/Veneam.svg';
+import sparret from '../images/Monsters/Sparret.svg';
 
 export default function Collection() {
   const [monsters, setMonsters] = useState([]);
@@ -33,11 +32,39 @@ export default function Collection() {
       const responseData = await response.json();
       const monstersArray = responseData.collection.map(item => ({
         name: item.monster.type.name,
-        picture: item.monster.type.picture
+        picture: getMonsterImage(item.monster.type.name) // Retrieve image based on monster's name
       }));
       setMonsters(monstersArray);
     } else {
       console.error("Failed to fetch collection");
+    }
+  };
+
+  // Function to get the imported image based on monster's name
+  const getMonsterImage = (monsterName) => {
+    switch (monsterName.toLowerCase()) {
+      case 'blob':
+        return blob;
+      case 'sockub':
+        return sockub;
+      case 'chickpick':
+        return chickpick;
+      case 'loom':
+        return loom;
+      case 'gunth':
+        return gunth;
+      case 'vorp':
+        return vorp;
+      case 'bonepos':
+        return bonepos;
+      case 'torrentoise':
+        return torrentoise;
+      case 'veneam':
+        return veneam;
+      case 'sparret':
+        return sparret;
+      default:
+        return null; // Return null for unsupported monsters or handle missing image case
     }
   };
 
@@ -60,7 +87,7 @@ export default function Collection() {
                 <h1 className="card-title">{monster.name}</h1>
               </div>
               <div className="card-header">
-                <img src={monster.name} />
+                <img src={monster.picture} alt={monster.name} />
               </div>
             </div>
           ))}
