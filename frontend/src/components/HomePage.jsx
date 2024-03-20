5bf3295d2e8d792b248bebc275dd28fe89e73c47
SMFreeze <117759431+smfreeze@users.noreply.github.com>
2024-03-20 14:15:49 +0000

    Update HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,38 +1,70 @@
-import favicon from '../images/favicon.svg';
-import "./general-styles.css";
-import React, { useEffect } from 'react';
+import HomePage from "./HomePage";
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
+import Map from "./Map";
+import Collection from "./Collection";
+import Account from "./Account";
+import Leaderboard from "./Leaderboard";
+import React, { useState } from "react";
+import "./navbar-style.css";
+import expand from '../images/Expand.svg'
+import back from '../images/Back.svg'
+import TermsAndConditions from "./TermsAndConditions";
 
-const HomePage = ({ setPage }) => {
-  useEffect(() => {
-    document.title = 'Home';
-  }, []);
+export default function App() {
+  const token = localStorage.getItem('token');
+  const isAuthenticated = !!token;
+  const [page, setPage] = useState(isAuthenticated ? 'map' : 'home');
+  const [toggleNav, setToggleNav] = useState(true);
+
+  const handleToggleNav = () => {
+    setToggleNav(prevToggleNav => !prevToggleNav);
+    document.getElementById("navbar").style.width = (toggleNav ? "98%" : "50px")
+  }
+
+  const renderPage = () => {
+    switch(page) {
+      case 'home':
+        return <HomePage setPage={setPage} />;
+      case 'signup':
+        return <SignUp setPage={setPage}/>;
+      case 'login':
+        return <LogIn setPage={setPage}/>;
+      case 'map':
+        return <Map setPage={setPage}/>;
+      case 'collection':
+        return <Collection setPage={setPage}/>;
+      case 'account':
+        return <Account setPage={setPage}/>;
+      case 'leaderboard':
+        return <Leaderboard setPage={setPage}/>;
+      case 'termsandconditions':
+        return <TermsAndConditions setPage={setPage}/>;
+    }
+  }
 
   return (
-    <div id="home-container" className="content-container">
-      <img id="logo" src={favicon} alt="logo" />
-      <div>
-        <br></br>
-        <button onClick={() => setPage('login')} className="btn btn-primary">
-          Log In
-        </button>
-        <button onClick={() => setPage('signup')} className="btn btn-primary">
-          Sign Up
-        </button>
-        <button onClick={() => setPage('map')} className="btn btn-primary">
-          [DEV] Map
-        </button>
-        <button onClick={() => setPage('collection')} className="btn btn-primary">
-          [DEV] Collection
-        </button>
-        <button onClick={() => setPage('account')} className="btn btn-primary">
-          [DEV] Account
-        </button>
-        <button onClick={() => setPage('leaderboard')} className="btn btn-primary">
-          [DEV] Leaderboard
-        </button>
-      </div>
+    <div>
+      {renderPage()}
+      {((page == 'collection') || (page == 'map') || (page == 'account') || (page == 'leaderboard')) && (
+        <div id='navbar'>
+          {!toggleNav && (
+            <div id="nav-buttons">
+              <button onClick={() => setPage('map')} >Map</button>
+              <button onClick={() => setPage('collection')} >Collection</button>
+              <button onClick={() => setPage('leaderboard')} >Leaderboard</button>
+              <button onClick={() => setPage('account')}>Account</button>
+            </div>
+          )}
+          <img src={expand} onClick={handleToggleNav}/>
+        </div>
+      )}
+      {((page == 'signup') || (page == 'login')) && (
+        <div className="btn btn-primary back" onClick={() => setPage('home')}>
+            <img src={back}/>
+            <p>Back</p>
+        </div>
+      )}
     </div>
   );
 }
-
-export default HomePage;
