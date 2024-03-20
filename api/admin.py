fd1d23c5972572849c0c90105ce1f7e7c7872988
unknown <rate.bekirov@gmail.com>
2024-03-20 20:25:20 +0000

    add note in admin page that first task of location should be quiz

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Collection, Monster, MonsterType, Task, TaskType, Location, LocationType
+from django import forms
+
+class LocationAdminForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        fields = '__all__'
+        help_texts = {
+            'task_slot1': 'Please ensure the first task slot contains a task of the type "quiz".',
+        }
+
+# Step 2: Custom admin class for the Location model using the custom form
+class LocationAdmin(admin.ModelAdmin):
+    form = LocationAdminForm
+    # You can add more customization here
+
+# Make sure to unregister the Location model if it was previously registered.
+admin.site.register(Location, LocationAdmin)
+
+
 
 admin.site.site_header = 'Admin Dashboard'
 admin.site.site_title = 'Admin Dashboard'
@@ -11,5 +30,4 @@ admin.site.register(Monster)
 admin.site.register(MonsterType)
 admin.site.register(Task)
 admin.site.register(TaskType)
-admin.site.register(Location)
 admin.site.register(LocationType)
