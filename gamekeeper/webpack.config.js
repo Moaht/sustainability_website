d8320a981f4df454fa60bc83b0ccb0a14e77bb9e
Thom <thom.allan03@googlemail.com>
2024-03-21 07:07:17 +0000

    Update gamekeeper, add gamekeeper login and task display

--- gamekeeper/webpack.config.js
+++ gamekeeper/webpack.config.js
@@ -2,12 +2,42 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader"
+        },
+        
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         },
       },
-    ],
+      {
+        test: /\.(sa|sc|c)ss$/, // styles files
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+      // {
+      //   test: /\.(png|svg)$/i,
+      //   loader: 'url-loader',
+      //   options: {
+      //     limit: false,
+      //     name: '[name].[hash:7].[ext]',
+      //     fallback: 'file-loader',
+      //     outputPath: '../images',
+      //     publicPath: '../images',
+      //   },
+      // },
+    ]
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx", ".es6"]
   },
 };
